8f3996c62e5fc61c6ef65a8d8795fa87758741a9
fanhl <dyguests@qq.com>
2018-04-14 23:41:23 +0800

    判断是不是情侣

--- src/q765_Couples_Holding_Hands/Q765.kt
+++ src/q765_Couples_Holding_Hands/Q765.kt
@@ -1,6 +1,7 @@
 package q765_Couples_Holding_Hands
 
 /**
+ * 情侣牵手
  *
  * https://leetcode-cn.com/problems/couples-holding-hands/description/
  *
@@ -24,7 +25,24 @@ object Q765 {
 
     class Solution {
         fun minSwapsCouples(row: IntArray): Int {
+            val sorted = isSorted(row)
+            println(sorted)
             return 0
+        }
+
+        private fun isSorted(row: IntArray): Boolean {
+            for (index in row.indices step 2) {
+                if (!isCouple(row, index)) return false
+            }
+            return true
+        }
+
+        private fun isCouple(row: IntArray, index: Int): Boolean {
+            return isCouple2(row[index - index % 2], row[index - index % 2 + 1])
+        }
+
+        private fun isCouple2(people1: Int, people2: Int): Boolean {
+            return people1 != people2 && people1 / 2 == people2 / 2
         }
     }
 }
