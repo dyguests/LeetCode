6e97e91d6d6b32c92ec682c85348ad21c9cf5d8a
fanhl <dyguests@qq.com>
2018-04-14 23:45:24 +0800

    另一半

--- src/q765_Couples_Holding_Hands/Q765.kt
+++ src/q765_Couples_Holding_Hands/Q765.kt
@@ -16,11 +16,19 @@ object Q765 {
         find(solution, 0, 1, 2, 3)
         find(solution, 0, 2, 1, 3)
         find(solution, 3, 2, 0, 1)
+
+        test(solution, 1)
+        test(solution, 233)
+        test(solution, 12)
     }
 
     private fun find(solution: Solution, vararg row: Int) {
         val result = solution.minSwapsCouples(row)
         println("$row => $result")
+    }
+
+    private fun test(solution: Solution, people: Int) {
+        println("$people->${solution.findCouple(people)}")
     }
 
     class Solution {
@@ -38,11 +46,18 @@ object Q765 {
         }
 
         private fun isCouple(row: IntArray, index: Int): Boolean {
-            return isCouple2(row[index - index % 2], row[index - index % 2 + 1])
+            return isCouple(row[index - index % 2], row[index - index % 2 + 1])
         }
 
-        private fun isCouple2(people1: Int, people2: Int): Boolean {
+        private fun isCouple(people1: Int, people2: Int): Boolean {
             return people1 != people2 && people1 / 2 == people2 / 2
+        }
+
+        /**
+         * 取得这个人的另一半
+         */
+        fun findCouple(people: Int): Int {
+            return people xor 1
         }
     }
 }
